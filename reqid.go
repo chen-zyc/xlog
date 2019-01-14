93c15ae93a451b853ccf0d951b969b6736f7c409
zhangyuchen <zhangyuchen0411@163.com>
2019-01-14 11:44:30 +0800

    :bug: ReqLogger 在使用 defaultLogger 作为 Logger 时，输出的文件位置不正确。

--- reqid.go
+++ reqid.go
@@ -34,6 +34,7 @@ type ReqConfig struct {
 type reqLogger struct {
 	ReqConfig
 	Logger
+	calldepth int
 }
 
 // NewReqLogger creates a ReqLogger.
@@ -42,16 +43,21 @@ func NewReqLogger(l Logger, c ReqConfig) ReqLogger {
 	if c.ReqID == "" && ReqIDGen != nil {
 		c.ReqID = ReqIDGen()
 	}
+	// Output() + Printx()
+	calldepth := 2
 	if l == nil {
 		l = defaultLogger
 		if c.Level == LevelPrint {
 			c.Level = l.CopyConfig().Level
 		}
+		// 因为 defaultLogger 是通过全局函数调用的，会多加一层，但这里是直接调用 defaultLogger，所以需要减掉一层。
+		calldepth--
 	}
 
 	return &reqLogger{
 		ReqConfig: c,
 		Logger:    l,
+		calldepth: calldepth,
 	}
 }
 
@@ -60,106 +66,106 @@ func (rl *reqLogger) RequestConfig() *ReqConfig {
 }
 
 func (rl *reqLogger) Print(v ...interface{}) {
-	_ = rl.Output(LevelPrint, 2, rl.ReqID, fmt.Sprint(v...))
+	_ = rl.Output(LevelPrint, rl.calldepth, rl.ReqID, fmt.Sprint(v...))
 }
 
 func (rl *reqLogger) Printf(format string, v ...interface{}) {
-	_ = rl.Output(LevelPrint, 2, rl.ReqID, fmt.Sprintf(format, v...))
+	_ = rl.Output(LevelPrint, rl.calldepth, rl.ReqID, fmt.Sprintf(format, v...))
 }
 
 func (rl *reqLogger) Println(v ...interface{}) {
-	_ = rl.Output(LevelPrint, 2, rl.ReqID, fmt.Sprintln(v...))
+	_ = rl.Output(LevelPrint, rl.calldepth, rl.ReqID, fmt.Sprintln(v...))
 }
 
 func (rl *reqLogger) Debug(v ...interface{}) {
 	if rl.Level <= LevelDebug {
-		_ = rl.Output(LevelDebug, 2, rl.ReqID, fmt.Sprint(v...))
+		_ = rl.Output(LevelDebug, rl.calldepth, rl.ReqID, fmt.Sprint(v...))
 	}
 }
 
 func (rl *reqLogger) Debugf(format string, v ...interface{}) {
 	if rl.Level <= LevelDebug {
-		_ = rl.Output(LevelDebug, 2, rl.ReqID, fmt.Sprintf(format, v...))
+		_ = rl.Output(LevelDebug, rl.calldepth, rl.ReqID, fmt.Sprintf(format, v...))
 	}
 }
 
 func (rl *reqLogger) Debugln(v ...interface{}) {
 	if rl.Level <= LevelDebug {
-		_ = rl.Output(LevelDebug, 2, rl.ReqID, fmt.Sprintln(v...))
+		_ = rl.Output(LevelDebug, rl.calldepth, rl.ReqID, fmt.Sprintln(v...))
 	}
 }
 
 func (rl *reqLogger) Info(v ...interface{}) {
 	if rl.Level <= LevelInfo {
-		_ = rl.Output(LevelInfo, 2, rl.ReqID, fmt.Sprint(v...))
+		_ = rl.Output(LevelInfo, rl.calldepth, rl.ReqID, fmt.Sprint(v...))
 	}
 }
 
 func (rl *reqLogger) Infof(format string, v ...interface{}) {
 	if rl.Level <= LevelInfo {
-		_ = rl.Output(LevelInfo, 2, rl.ReqID, fmt.Sprintf(format, v...))
+		_ = rl.Output(LevelInfo, rl.calldepth, rl.ReqID, fmt.Sprintf(format, v...))
 	}
 }
 
 func (rl *reqLogger) Infoln(v ...interface{}) {
 	if rl.Level <= LevelInfo {
-		_ = rl.Output(LevelInfo, 2, rl.ReqID, fmt.Sprintln(v...))
+		_ = rl.Output(LevelInfo, rl.calldepth, rl.ReqID, fmt.Sprintln(v...))
 	}
 }
 
 func (rl *reqLogger) Warn(v ...interface{}) {
 	if rl.Level <= LevelWarn {
-		_ = rl.Output(LevelWarn, 2, rl.ReqID, fmt.Sprint(v...))
+		_ = rl.Output(LevelWarn, rl.calldepth, rl.ReqID, fmt.Sprint(v...))
 	}
 }
 
 func (rl *reqLogger) Warnf(format string, v ...interface{}) {
 	if rl.Level <= LevelWarn {
-		_ = rl.Output(LevelWarn, 2, rl.ReqID, fmt.Sprintf(format, v...))
+		_ = rl.Output(LevelWarn, rl.calldepth, rl.ReqID, fmt.Sprintf(format, v...))
 	}
 }
 
 func (rl *reqLogger) Warnln(v ...interface{}) {
 	if rl.Level <= LevelWarn {
-		_ = rl.Output(LevelWarn, 2, rl.ReqID, fmt.Sprintln(v...))
+		_ = rl.Output(LevelWarn, rl.calldepth, rl.ReqID, fmt.Sprintln(v...))
 	}
 }
 
 func (rl *reqLogger) Error(v ...interface{}) {
 	if rl.Level <= LevelError {
-		_ = rl.Output(LevelError, 2, rl.ReqID, fmt.Sprint(v...))
+		_ = rl.Output(LevelError, rl.calldepth, rl.ReqID, fmt.Sprint(v...))
 	}
 }
 
 func (rl *reqLogger) Errorf(format string, v ...interface{}) {
 	if rl.Level <= LevelError {
-		_ = rl.Output(LevelError, 2, rl.ReqID, fmt.Sprintf(format, v...))
+		_ = rl.Output(LevelError, rl.calldepth, rl.ReqID, fmt.Sprintf(format, v...))
 	}
 }
 
 func (rl *reqLogger) Errorln(v ...interface{}) {
 	if rl.Level <= LevelError {
-		_ = rl.Output(LevelError, 2, rl.ReqID, fmt.Sprintln(v...))
+		_ = rl.Output(LevelError, rl.calldepth, rl.ReqID, fmt.Sprintln(v...))
 	}
 }
 
 func (rl *reqLogger) Fatal(v ...interface{}) {
 	if rl.Level <= LevelFatal {
-		_ = rl.Output(LevelFatal, 2, rl.ReqID, fmt.Sprint(v...))
+		_ = rl.Output(LevelFatal, rl.calldepth, rl.ReqID, fmt.Sprint(v...))
 		osExit(1)
 	}
 }
 
 func (rl *reqLogger) Fatalf(format string, v ...interface{}) {
 	if rl.Level <= LevelFatal {
-		_ = rl.Output(LevelFatal, 2, rl.ReqID, fmt.Sprintf(format, v...))
+		_ = rl.Output(LevelFatal, rl.calldepth, rl.ReqID, fmt.Sprintf(format, v...))
 		osExit(1)
 	}
 }
 
 func (rl *reqLogger) Fatalln(v ...interface{}) {
 	if rl.Level <= LevelFatal {
-		_ = rl.Output(LevelFatal, 2, rl.ReqID, fmt.Sprintln(v...))
+		_ = rl.Output(LevelFatal, rl.calldepth, rl.ReqID, fmt.Sprintln(v...))
 		osExit(1)
 	}
 }
@@ -167,7 +173,7 @@ func (rl *reqLogger) Fatalln(v ...interface{}) {
 func (rl *reqLogger) Panic(v ...interface{}) {
 	if rl.Level <= LevelPanic {
 		s := fmt.Sprint(v...)
-		_ = rl.Output(LevelPanic, 2, rl.ReqID, s)
+		_ = rl.Output(LevelPanic, rl.calldepth, rl.ReqID, s)
 		panic(s)
 	}
 }
@@ -175,7 +181,7 @@ func (rl *reqLogger) Panic(v ...interface{}) {
 func (rl *reqLogger) Panicf(format string, v ...interface{}) {
 	if rl.Level <= LevelPanic {
 		s := fmt.Sprintf(format, v...)
-		_ = rl.Output(LevelPanic, 2, rl.ReqID, s)
+		_ = rl.Output(LevelPanic, rl.calldepth, rl.ReqID, s)
 		panic(s)
 	}
 }
@@ -183,7 +189,7 @@ func (rl *reqLogger) Panicf(format string, v ...interface{}) {
 func (rl *reqLogger) Panicln(v ...interface{}) {
 	if rl.Level <= LevelPanic {
 		s := fmt.Sprintln(v...)
-		_ = rl.Output(LevelPanic, 2, rl.ReqID, s)
+		_ = rl.Output(LevelPanic, rl.calldepth, rl.ReqID, s)
 		panic(s)
 	}
 }
